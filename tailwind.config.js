69af9859e51932c7cfa9e1670689b8edfd96e946
wontak12 <168093256+wontak12@users.noreply.github.com>
2025-08-10 23:49:09 +0900

    taiolwind 설정 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./**/*.{html,js,jsx}'],
+module.exports = {
+  content: [
+    './index.html', // HTML
+    './src/**/*.{js,jsx,ts,tsx}', // src 폴더 내 JS/TSX 파일만
+  ],
   theme: {
     extend: {},
   },
