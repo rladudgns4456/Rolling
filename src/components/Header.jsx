4413add5032740666d4dd60df44a39cf986f7207
leohan6540 <leohan6540@gmail.com>
2025-08-11 12:28:07 +0900

    테일윈드 세팅 및 헤더 수정

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="flex justify-between bg-blue-900 text-cyan-50">
+      <div>Rolling</div>
+      <div>
+        <button className="">롤링 페이퍼 만들기</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
