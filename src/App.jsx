74c91319d9c15ca0dd0e26bd946cb3103abb30cc
wontak12 <168093256+wontak12@users.noreply.github.com>
2025-08-12 15:17:00 +0900

    배경이미지 적용(정적)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+const BaseURL = 'https://rolling-api.vercel.app/';
 
-import Card from './components/Card';
+import CardListCard from './components/CardListCard';
 
 function App() {
+  const [bgImageUrl, setBgImageUrl] = useState([]);
+  useEffect(() => {
+    fetch(`${BaseURL}background-images/`, { method: 'GET' })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('API 응답 데이터:', data);
+        if (data.imageUrls) {
+          setBgImageUrl(data.imageUrls);
+          console.log(data.imageUrls);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
   return (
-    <div className="cardList flex gap-[10px]">
-      <Card bgColor="bg-purple1" color="purple" name="Sowon" />
-      <Card bgColor="bg-beige2" color="beige" />
-      <Card bgColor="bg-blue2" color="blue" />
-      <Card bgColor="bg-green1" color="green" />
+    <div className="cardList grid grid-cols-4 gap-[10px]">
+      <CardListCard bgColor="bg-purple1" color="purple" name="Sowon" />
+      <CardListCard bgColor="bg-beige2" color="beige" name="Sowon" />
+      <CardListCard bgColor="bg-blue2" color="blue" name="Sowon" />
+      <CardListCard bgColor="bg-green1" color="green" name="Sowon" />
+      <CardListCard
+        bgColor="bg-purple1"
+        color="purple"
+        name="Sowon"
+        bgImageUrl={bgImageUrl[0]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
+      />
+      <CardListCard
+        bgColor="bg-purple1"
+        color="purple"
+        name="Sowon"
+        bgImageUrl={bgImageUrl[1]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
+      />
+      <CardListCard
+        bgColor="bg-purple1"
+        color="purple"
+        name="Sowon"
+        bgImageUrl={bgImageUrl[2]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
+      />
+      <CardListCard
+        bgColor="bg-purple1"
+        color="purple"
+        name="Sowon"
+        bgImageUrl={bgImageUrl[3]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
+      />
     </div>
   );
 }
---
+++ src/components/CardListCard.jsx
@@ -0,0 +1,24 @@
+import Emoges from './Emoge';
+import ProfileCount from './profileCount';
+
+import Shadow from './Shadow';
+
+export default function CardListCard({ bgColor, color, name, bgImageUrl }) {
+  return (
+    <>
+      <div
+        className={`bg-center bg-cover cardList relative z-1 ${bgColor} w-[275px] h-[260px] pt-[30px] pr-[24px] pb-[20px] pl-[24px] rounded-[16px]  border border-[#0000001A] shadow-[0px_2px_12px_0px_#00000014]`}
+        style={{ backgroundImage: `url(${bgImageUrl})` }}
+      >
+        <div className="p-0 w-[227px] h-[210px]">
+          <span className="z-10 flex justify-start mb-3  text-[24px] font-bold leading-9 tracking-[-0.01em]">
+            To. {name}
+          </span>
+          <ProfileCount totalCount="30" isColumn={true} />
+          <Emoges className="flex z-10 pt-[17px] pr-[28px] gap-[10px] mt-[43px] w-[227px] h-[53px] border-t border-[#0000001F]" />
+          {!bgImageUrl && <Shadow color={color} />}
+        </div>
+      </div>
+    </>
+  );
+}
