90d5ee2142fe696be3de0bdc0329b26be9866eb8
rladudgns4456 <92049252+rladudgns4456@users.noreply.github.com>
2025-08-12 18:33:17 +0900

    dev 충돌 오류 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
+import UserRollingContainer from './components/UserRollingContainer';
+import BackgroundSelect from './components/BackgroundSelect';
+import UserProfilePreview from './components/UserProfilePreview';
 import './App.css';
+
 const BaseURL = 'https://rolling-api.vercel.app/';
 
 import CardListCard from './components/CardListCard';
@@ -19,43 +23,38 @@ function App() {
       .catch((err) => console.error(err));
   }, []);
   return (
-    <div className="cardList grid grid-cols-4 gap-[10px]">
-      <CardListCard bgColor="bg-purple1" color="purple" name="Sowon" />
-      <CardListCard bgColor="bg-beige2" color="beige" name="Sowon" />
-      <CardListCard bgColor="bg-blue2" color="blue" name="Sowon" />
-      <CardListCard bgColor="bg-green1" color="green" name="Sowon" />
-      <CardListCard
-        bgColor="bg-purple1"
-        color="purple"
-        name="Sowon"
-        bgImageUrl={bgImageUrl[0]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
-      />
-      <CardListCard
-        bgColor="bg-purple1"
-        color="purple"
-        name="Sowon"
-        bgImageUrl={bgImageUrl[1]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
-      />
-      <CardListCard
-        bgColor="bg-purple1"
-        color="purple"
-        name="Sowon"
-        bgImageUrl={bgImageUrl[2]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
-      />
-      <CardListCard
-        bgColor="bg-purple1"
-        color="purple"
-        name="Sowon"
-        bgImageUrl={bgImageUrl[3]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
-      />
-    </div>
-import UserRollingContainer from './components/UserRollingContainer';
-import BackgroundSelect from './components/BackgroundSelect';
-import UserProfilePreview from './components/UserProfilePreview';
-
-function App() {
-  return (
     <>
+      {' '}
+      <div className="cardList grid grid-cols-4 gap-[10px]">
+        <CardListCard bgColor="bg-purple1" color="purple" name="Sowon" />
+        <CardListCard bgColor="bg-beige2" color="beige" name="Sowon" />
+        <CardListCard bgColor="bg-blue2" color="blue" name="Sowon" />
+        <CardListCard bgColor="bg-green1" color="green" name="Sowon" />
+        <CardListCard
+          bgColor="bg-purple1"
+          color="purple"
+          name="Sowon"
+          bgImageUrl={bgImageUrl[0]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
+        />
+        <CardListCard
+          bgColor="bg-purple1"
+          color="purple"
+          name="Sowon"
+          bgImageUrl={bgImageUrl[1]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
+        />
+        <CardListCard
+          bgColor="bg-purple1"
+          color="purple"
+          name="Sowon"
+          bgImageUrl={bgImageUrl[2]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
+        />
+        <CardListCard
+          bgColor="bg-purple1"
+          color="purple"
+          name="Sowon"
+          bgImageUrl={bgImageUrl[3]} //길이는 0~3까지만 4부터는 아무것도없음 나중에 수정
+        />
+      </div>
       <div className="px-5 sm:px-6 xl:px-10 w-max-[1280px]">
         <UserRollingContainer />
         <BackgroundSelect />
