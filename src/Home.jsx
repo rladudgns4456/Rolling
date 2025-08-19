d9be4a8aaf836010231d4bcd8c2c43f8cf72acdf
rladudgns4456 <92049252+rladudgns4456@users.noreply.github.com>
2025-08-19 15:39:14 +0900

    메인 페이지 최종 수정

--- src/Home.jsx
+++ src/Home.jsx
@@ -81,7 +81,6 @@ export default function MainPage() {
           </div>
         </section>
 
-        {/* CTA */}
         <div className="flex justify-center">
           <TextButton
             variant="primary"
