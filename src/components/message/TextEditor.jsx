32e237e62880a77950306b98510c82c813b3f492
leohan6540 <leohan6540@gmail.com>
2025-08-23 18:06:05 +0900

    message 페이지 최종 완성

--- src/components/message/TextEditor.jsx
+++ src/components/message/TextEditor.jsx
@@ -7,7 +7,7 @@ import { EDITOR_FORMATS } from '../../constants/EDITOR_FORMATS';
 
 const TextEditor = ({ className, onChange }) => {
   const [value, setValue] = useState(''); // HTML 값 (에디터용)
-  const [plainText, setPlainText] = useState(''); // 태그 제거된 값 (DB 저장용)
+  //const [plainText, setPlainText] = useState(''); // 태그 제거된 값 (DB 저장용)
 
   const modules = {
     toolbar: {
@@ -24,7 +24,6 @@ const TextEditor = ({ className, onChange }) => {
         onChange={(content, delta, source, editor) => {
           setValue(content); // ReactQuill 내부에는 HTML 그대로 유지
           const textOnly = editor.getText().trim();
-          setPlainText(textOnly); // hidden input 용 값 업데이트
 
           if (onChange) {
             onChange(textOnly); // 부모 컴포넌트에도 plain text 전달
